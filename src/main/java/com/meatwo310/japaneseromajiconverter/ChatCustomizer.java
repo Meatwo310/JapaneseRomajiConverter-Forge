b7822bfe90c5317d76c449fe00805107aa296168
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2023-08-07 21:23:13 +0900

    サーバーからではなくプレイヤーからメッセージを送信するように変更

--- src/main/java/com/meatwo310/japaneseromajiconverter/ChatCustomizer.java
+++ src/main/java/com/meatwo310/japaneseromajiconverter/ChatCustomizer.java
@@ -1,79 +1,85 @@
 package com.meatwo310.japaneseromajiconverter;
 
-import com.mojang.logging.LogUtils;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.server.ServerLifecycleHooks;
-import org.slf4j.Logger;
 
 @Mod.EventBusSubscriber
 public class ChatCustomizer {
-    private static final Logger LOGGER = LogUtils.getLogger();
+//    private static final Logger LOGGER = LogUtils.getLogger();
 
     @SubscribeEvent
     public static void onChat(ServerChatEvent.Submitted event) {
-        ServerPlayer entity = event.getPlayer();
-        String text = event.getRawText();
-        LevelAccessor world = entity.level;
+        ServerPlayer player = event.getPlayer();
+        String baseText = event.getRawText();
+        LevelAccessor world = player.level;
 
-        // クライアントサイドでは重複実行させない
+        // クライアントサイドでは実行しない
+        // 注: シングルプレイでも内部サーバーが建っているためこのMODは機能する
         if (world.isClientSide()) {
             return;
         }
 
-        String resultText = "<" + entity.getDisplayName().getString() + "> ";
-
-        // TODO: コンポーネントについて調べる
+//        String resultText = "<" + entity.getDisplayName().getString() + "> ";
+        String resultText;
 
         // 空文字の場合は変換しない
-        if (text.isEmpty()) {
-//            LOGGER.debug("text is empty");
-            resultText += text;
+        if (baseText.isEmpty()) {
+//            LOGGER.debug("baseText is empty");
+//            resultText += baseText;
+            return;
         }
         // 短すぎる場合は変換しない(\や¥などで始まる場合は変換)
-        else if (text.length() < 5 && !text.matches("^[\\\\¥￥].*$")) {
-//            LOGGER.debug("text is too short");
-            resultText += text;
+        else if (baseText.length() < 5 && !baseText.matches("^[\\\\¥￥].*$")) {
+//            LOGGER.debug("baseText is too short");
+//            resultText += baseText;
+            return;
         }
         // :で始まる場合は変換しない
-        else if (text.startsWith(":")) {
-//            LOGGER.debug("text starts with :");
-            resultText += text;
+        else if (baseText.startsWith(":")) {
+//            LOGGER.debug("baseText starts with :");
+//            resultText += baseText;
+            return;
         }
         // ASCII文字や\¥￥以外の文字が含まれる場合は変換しない
-        else if (!text.matches("^[!-~\\s¥￥]+$")) {
-//            LOGGER.debug("text contains non-ASCII characters");
-            resultText += text;
+        else if (!baseText.matches("^[!-~\\s¥￥]+$")) {
+//            LOGGER.debug("baseText contains non-ASCII characters");
+//            resultText += baseText;
+            return;
         }
         // !#;で始まる場合は変換しないが、頭文字を取り除いて括弧付きで元のメッセージを示す
-        else if (text.matches("^[!#;].*$")) {
-//            LOGGER.debug("text starts with !#;");
-            resultText += text.substring(1) + " §7(" + text + ")";
+        else if (baseText.matches("^[!#;].*$")) {
+//            LOGGER.debug("baseText starts with !#;");
+            resultText = baseText.substring(1) + " §7(" + baseText + ")";
         }
         // \¥￥で始まる場合は、頭文字を取り除いて変換し、括弧付きで元のメッセージを示す
-        else if (text.matches("^[\\\\¥￥].*$")) {
-//            LOGGER.debug("text starts with \\¥￥");
-            resultText += RomajiToHiragana.convert(text.substring(1)) + " §7(" + text + ")";
+        else if (baseText.matches("^[\\\\¥￥].*$")) {
+//            LOGGER.debug("baseText starts with \\¥￥");
+            resultText = RomajiToHiragana.convert(baseText.substring(1)) + " §7(" + baseText + ")";
         }
         // 条件に当てはまらない普通のローマ字メッセージは変換
         else {
-//            LOGGER.debug("text is normal");
-            resultText += RomajiToHiragana.convert(text) + " §7(" + text + ")";
+//            LOGGER.debug("baseText is normal");
+            resultText = RomajiToHiragana.convert(baseText) + " §7(" + baseText + ")";
         }
 
+        /*
         MinecraftServer _mcserv = ServerLifecycleHooks.getCurrentServer();
         if (_mcserv != null) {
             _mcserv.getPlayerList().broadcastSystemMessage(Component.literal(resultText), false);
         }
-//        entity.sendSystemMessage(Component.literal(resultText));
+        entity.sendSystemMessage(Component.literal(resultText));
+        */
 
+        event.setMessage(Component.literal(resultText));
+
+        /*
         if (event.isCancelable()) {
             event.setCanceled(true);
         }
+        */
     }
 }
